19c47264f32034f8aab4e07b944c7e104030761e
ashudeep <ashudeep21@gmail.com>
2024-09-17 23:26:58 -0700

    Refactor code, send rerank methods to a new file

--- dataset_utils.py
+++ dataset_utils.py
@@ -7,6 +7,16 @@ class Dataset:
         self.df = pd.read_csv(file_path, sep="\t")
         self.df["TuringMMTextEmbedding"] = self.df["TuringMMTextEmbedding"].apply(eval)
 
+    def get_doc_embedding(self, query, murl_key):
+        return self.df[(self.df["Query"] == query) & (self.df["MUrlKey"] == murl_key)][
+            "TuringMMTextEmbedding"
+        ].values[0]
+
+    def get_doc_embedding_batch(self, query, murl_keys):
+        return self.df[
+            (self.df["Query"] == query) & (self.df["MUrlKey"].isin(murl_keys))
+        ]["TuringMMTextEmbedding"].values.tolist()
+
     def get_nearest_neighbors(self, query, murl_key, k=5):
         """
         Get the k nearest neighbors of a given (query, murlkey) pair
@@ -15,9 +25,7 @@ class Dataset:
             raise IndexError(f"Query={query} not found in the dataset")
         if murl_key not in self.df[self.df["Query"] == query]["MUrlKey"].values:
             raise IndexError("MUrlKey not found in the dataset for the given query")
-        query_embedding = self.df[self.df["Query"] == query][
-            self.df["MUrlKey"] == murl_key
-        ]["TuringMMTextEmbedding"].values[0]
+        query_embedding = self.get_doc_embedding(query, murl_key)
 
         other_docs = self.df[self.df["Query"] == query][self.df["MUrlKey"] != murl_key]
         if other_docs.empty:
@@ -34,6 +42,7 @@ class Dataset:
         most_similar_doc_indices = cosine_similarities.argsort()[0][::-1][:k]
         most_similar_docs = other_docs.iloc[most_similar_doc_indices]
         return most_similar_docs
+
 
 if __name__ == "__main__":
     dataset = Dataset()
---
+++ rerank_methods.py
@@ -0,0 +1,35 @@
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+
+def simple_rerank(doc_list, liked_doc_list, disliked_doc_list):
+    # Example ranking logic: prioritize liked docs, exclude disliked docs
+    liked_docs = [doc for doc in doc_list if doc["doc_id"] in liked_doc_list]
+    neutral_docs = [doc for doc in doc_list if doc["doc_id"] not in liked_doc_list + disliked_doc_list]
+    return [doc["doc_id"] for doc in liked_docs + neutral_docs]
+
+
+def score_based_rerank(query, doc_list, liked_doc_list, disliked_doc_list, dataset):
+    # Need to boost the ranking of documents that are similar to liked documents,
+    embeddings_liked_docs = dataset.get_doc_embedding_batch(query, liked_doc_list)
+    embeddings_disliked_docs = dataset.get_doc_embedding_batch(query, disliked_doc_list) 
+    # Need to penalize the ranking of documents that are similar to disliked documents
+    all_embeddings = dataset.get_doc_embedding_batch(query, doc_list)
+    
+    if liked_doc_list:
+        # Calculate similarity between all_embeddings and embeddings_liked_docs, and take the highest distance
+        liked_docs_similarities = cosine_similarity(all_embeddings, embeddings_liked_docs)
+        best_liked_doc_similarity = np.max(liked_docs_similarities, axis=1)
+    else:
+        best_liked_doc_similarity = np.zeros(len(doc_list))
+    if disliked_doc_list:
+        # Calculate similarity between all_embeddings and embeddings_disliked_docs
+        dislked_docs_similarities = cosine_similarity(all_embeddings, embeddings_disliked_docs)
+        best_disliked_doc_similarity = np.max(dislked_docs_similarities, axis=1)
+    else:
+        best_disliked_doc_similarity = np.zeros(len(doc_list))
+
+    # Calculate the score for each document
+    all_docs_score = best_liked_doc_similarity - best_disliked_doc_similarity
+    ranked_doc_indices = np.argsort(all_docs_score)[::-1]
+    return [doc_list[i] for i in ranked_doc_indices]
