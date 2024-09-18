b4450a0ab1cbafcac8192b47c5e137007683d684
Chenyang Cui <checui@microsoft.com>
2024-09-17 18:14:49 -0700

    change to service

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/rank', methods=['POST'])
 def rank_docs():
@@ -50,5 +53,6 @@ def generate_suggested_query(query, liked_others, dislike_others):
 
     return suggested_query
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
